854d82e2a7b537ed2c888d0b777b50eb0867909c
EfratVardi <211.efrat@gmail.com>
2025-01-31 13:33:11 +0200

    add files

--- DTO/obj/Debug/net7.0/DTO.AssemblyInfo.cs
+++ DTO/obj/Debug/net7.0/DTO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0219835f6a16d8b60308dbe6f8ec84dc77f43c48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99020017df566d2a48e374d2e120a06860630de9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
